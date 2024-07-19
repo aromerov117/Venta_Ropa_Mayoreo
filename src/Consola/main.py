d3b9f309b69e0c0b1f89fdfdf7a0bcee0f3e6878
Angel RV <aromerov117@gmail.com>
2024-07-19 11:40:55 -0600

    Menu por roles

--- src/Consola/main.py
+++ src/Consola/main.py
@@ -1,30 +1,40 @@
+# main.py
+
 from registro_usuario import registrar_usuario, visualizar_usuarios
 from inicio_sesion import iniciar_sesion
-from ventas import *
-from compras import mostrar_historial_compras
 from contabilidad import mostrar_analisis_contabilidad
 from catalogo import mostrar_catalogo
 from compra_venta import *
-from modelos import Producto
 from gestion_proveedores import *
 from edicion_gestion_usuarios import *
-from registro_producto import Producto
 from gestion_inventario import menu_gestion_inventario
 from registro_producto import productos_registrados
 
-opciones_menu = [
-    "Catalogo",
-    "Entrada de Productos en Inventario",
-    "Salida de Productos del Inventario",
-    "Registro de Devoluciones de Productos al Proveedor",
-    "Gestión de Proveedores y Contactos",
-    "Gestión de Ajustes de Inventario",
-    "Historial de Ventas",
-    "Historial de Compras",
-    "Análisis de Contabilidad de Compras y Ventas",
-    "Visualizar usuarios",
-    "Gestionar usuarios"
-]
+menus = {
+    "administrador": [
+        "Manejo y administrador de roles",
+        "Desbloqueo de cuentas",
+        "Recuperacion de respaldos",
+        "Visualizar usuarios",
+        "Gestionar usuarios"
+    ],
+    "gerente": [
+        "Catalogo",
+        "Entrada de Productos en Inventario",
+        "Salida de Productos del Inventario",
+        "Registro de Devoluciones de Productos al Proveedor",
+        "Gestión de Proveedores y Contactos",
+        "Gestión de Ajustes de Inventario",
+        "Historial de Ventas",
+        "Historial de Compras",
+        "Análisis de Contabilidad de Compras y Ventas",
+        "Visualizar usuarios"
+    ],
+    "vendedor": [
+        "Catalogo",
+        "Salida de productos"
+    ]
+}
 
 def interfaz_registro():
     print("Bienvenido al registro de usuarios.")
@@ -41,25 +51,25 @@ def interfaz_inicio_sesion():
     correo_electronico = input("Ingresa tu correo electrónico: ")
     contrasena = input("Ingresa tu contraseña: ")
 
-    iniciado, mensaje = iniciar_sesion(correo_electronico, contrasena)
+    iniciado, mensaje, rol = iniciar_sesion(correo_electronico, contrasena)
     if iniciado:
         print(mensaje)
-        mostrar_menu()
+        mostrar_menu(rol)
     else:
         print(mensaje)
 
-def mostrar_menu():
+def mostrar_menu(rol):
     while True:
-        print("\nMenú de opciones:")
-        for i, opcion in enumerate(opciones_menu, start=1):
+        print(f"\nMenú de opciones para {rol}:")
+        for i, opcion in enumerate(menus[rol], start=1):
             print(f"{i}. {opcion}")
 
         opcion = input("Selecciona una opción del menú (o 'q' para salir): ")
 
         if opcion.isdigit():
             opcion = int(opcion)
-            if 1 <= opcion <= len(opciones_menu):
-                seleccionar_opcion(opcion)
+            if 1 <= opcion <= len(menus[rol]):
+                seleccionar_opcion(opcion, rol)
             else:
                 print("Opción no válida. Por favor, intenta de nuevo.")
         elif opcion.lower() == 'q':
@@ -68,32 +78,49 @@ def mostrar_menu():
         else:
             print("Opción no válida. Por favor, intenta de nuevo.")
 
-def seleccionar_opcion(opcion):
-    if opcion == 1:
-        mostrar_catalogo()
-    elif opcion == 2:
-        mostrar_entrada_productos(productos_registrados, proveedores, compras_realizadas)
-    elif opcion == 3:
-        mostrar_salida_productos(productos_registrados,ventas_realizadas)
-    elif opcion == 4:
-        menu_proveedores(proveedores)
-    elif opcion == 5:
-        menu_proveedores(proveedores)
-    elif opcion == 6:
-        menu_gestion_inventario()
-    elif opcion == 7:
-        mostrar_historial_ventas()
-    elif opcion == 8:
-        mostrar_historial_compras(compras_realizadas)
-    elif opcion == 9:
-        mostrar_analisis_contabilidad()
-    elif opcion == 10:
-        visualizar_usuarios()
-    elif opcion == 11:
-        editar_usuario()
-    else:
-        print(f"Seleccionaste: {opciones_menu[opcion - 1]}")
-        # Aquí puedes agregar la lógica para redirigir al usuario al módulo correspondiente
+def seleccionar_opcion(opcion, rol):
+    if rol == "administrador":
+        if opcion == 1:
+            # Implementar la funcionalidad de manejo y administrador de roles
+            pass
+        elif opcion == 2:
+            # Implementar la funcionalidad de desbloqueo de cuentas
+            pass
+        elif opcion == 3:
+            # Implementar la funcionalidad de recuperación de respaldos
+            pass
+        elif opcion == 4:
+            visualizar_usuarios()
+        elif opcion == 5:
+            editar_usuario()
+
+    elif rol == "gerente":
+        if opcion == 1:
+            mostrar_catalogo()
+        elif opcion == 2:
+            mostrar_entrada_productos(productos_registrados, proveedores, compras_realizadas)
+        elif opcion == 3:
+            mostrar_salida_productos(productos_registrados, ventas_realizadas)
+        elif opcion == 4:
+            registrar_devolucion_productos(proveedores)
+        elif opcion == 5:
+            menu_proveedores(proveedores)
+        elif opcion == 6:
+            menu_gestion_inventario()
+        elif opcion == 7:
+            mostrar_historial_ventas()
+        elif opcion == 8:
+            mostrar_historial_compras(compras_realizadas)
+        elif opcion == 9:
+            mostrar_analisis_contabilidad()
+        elif opcion == 10:
+            visualizar_usuarios()
+
+    elif rol == "vendedor":
+        if opcion == 1:
+            mostrar_catalogo()
+        elif opcion == 2:
+            mostrar_salida_productos(productos_registrados, ventas_realizadas)
 
 if __name__ == "__main__":
     while True:
